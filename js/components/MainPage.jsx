8788e1d14e8330cf97762e16d336e896072bd266
Costin Glejaru <costincosmin12@gmail.com>
2023-04-30 01:22:03 +0300

    secondary commit to save

---
+++ js/components/MainPage.jsx
@@ -0,0 +1,8 @@
+// js/components/MainPage.jsx
+export default function MainPage() {
+	return (
+		<div>
+            Termin proiectul asta la iarna
+        </div>
+	)
+}
---
+++ pages/insert.js
@@ -0,0 +1,10 @@
+// pages/insert.js
+
+import InsertPage from "@/js/components/InsertPage";
+
+
+export default function Insert() {
+  return (
+      <InsertPage/>
+  )
+}
