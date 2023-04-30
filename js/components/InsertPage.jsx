299de76d2e5492a148cee4e1b6ebdaaf4aff34ea
Costin Glejaru <costincosmin12@gmail.com>
2023-04-30 12:13:45 +0300

    front end

--- js/components/InsertPage.jsx
+++ js/components/InsertPage.jsx
@@ -1,6 +1,75 @@
 // js/components/InsertPage.jsx
+import { useState } from 'react';
+
 export default function InsertPage() {
-	return (
-		<div></div>
-	)
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  const insertRecord = (event) => {
+    event.preventDefault();
+    const data = { name, description };
+    fetch('/api/records', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then(() => {
+      console.log('New record inserted');
+      setName('');
+      setDescription('');
+    });
+  };
+
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="container px-6 py-10 mx-auto">
+        <h1 className="w-[500px] mx-auto text-center text-6xl">Cloud Computing App</h1>
+        <p className="w-[1000px] mx-auto text-center mt-4 text-3xl">
+          This app helps user to find info's about artist
+        </p>
+
+        <form>
+          <div className="mb-6">
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Artist name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="description"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Artist description
+            </label>
+            <textarea
+              id="description"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            onClick={insertRecord}
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
--- js/components/MainPage.jsx
+++ js/components/MainPage.jsx
@@ -1,44 +1,49 @@
-// js/components/MainPage.jsx
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function MainPage() {
-	const [records, setRecords] = useState([]);
+  const [records, setRecords] = useState([]);
 
-	useEffect(() => {
-		try{
-			fetch('/api/records', {
-				method: 'GET',
-			})
-				.then(response => response.json())
-				.then(json => setRecords(json.data));
-		}
-		catch (error) {
-			console.log(error);
-		}
-	}, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-	const deleteRecord = (event) => {
-		event.preventDefault();
-		const id = event.target.id;
-		try {
-			fetch(`/api/records?id=${id}`, {
-				method: 'DELETE',
-			})
-				.then(response => response.json())
-				.then(json => {
-						setRecords(records.filter(record => record._id !== id));
-				});
-		}
-		catch (error) {
-			console.log(error);
-		}
-	}
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/api/records', {
+        method: 'GET',
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setRecords(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteRecord = async (event) => {
+    event.preventDefault();
+    const id = event.target.id;
+    try {
+      const response = await fetch(`/api/records?id=${id}`, {
+        method: 'DELETE',
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      setRecords(records.filter(record => record._id !== id));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
 
 	return (
 		<section className="bg-white dark:bg-gray-900">
 			<div className="container px-6 py-10 mx-auto">
-				<h1 className="w-[500px] mx-auto text-center text-6xl">Fun facts app</h1>
-				<p className="w-[1000px] mx-auto text-center mt-4 text-3xl">This is an app that showcases fun facts</p>
+				<h1 className="w-[500px] mx-auto text-center text-6xl">Cloud Computing Project</h1>
+				<p className="w-[1000px] mx-auto text-center mt-4 text-3xl">Info's about artists</p>
 
 				<div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
 					{records.map(record => (
@@ -46,7 +51,7 @@ export default function MainPage() {
 							key={record._id}
 							className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 							<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-								{record.title}
+								{record.name}
 							</h5>
 							<p className="font-normal text-gray-700 dark:text-gray-400">
 								{record.description}
